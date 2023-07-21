a59db70c67b5b5ea319d9fc2eaab225e4af7213f
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-07-21 14:52:57 +0100

    understaning constructors

---
+++ 2_constructor.js
@@ -0,0 +1,12 @@
+//  constructor
+
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
+
+const book1 = new Book('Book One', 'jone doe', '2022');
+const book2 = new Book('Book two', 'job cak', '2034');
+
+console.log(book1);
