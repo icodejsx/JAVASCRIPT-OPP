65e27a1ac7afc316150d155d03ad60b571994359
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-07-25 14:49:05 +0100

    learning

---
+++ 3_prototype.js
@@ -0,0 +1,20 @@
+//  constructor
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+    this.getSummary = function () {
+        return `${this.title} was written by ${this.author} on ${this.year}`
+    }
+}
+
+// getSummary 
+Book.prototype.getSummary
+
+
+// instanciate an Object
+const book1 = new Book('Book One', 'jone doe', '2022');
+const book2 = new Book('Book two', 'job cak', '2034');
+
+console.log(book1.getSummary());
