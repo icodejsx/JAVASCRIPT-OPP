1b4e16c97ac957feaf091357ae6902cd9ea685d7
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-07-26 11:30:23 +0100

    LEARNING

---
+++ 6_classes.js
@@ -0,0 +1,16 @@
+class Book {
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+
+    }
+    getSummary() {
+        return `${this.title} was written by ${this.author} on ${this.year}`
+    }
+}
+
+// instantiating Object 
+const book1 = new Book("Book one", "jondo", '2019')
+
+console.log(book1)
